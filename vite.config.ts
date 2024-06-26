8d2cc3a486641d39e3354c8e30e21379858f2720
songsongyl <2444616264@qq.com>
2024-06-26 19:06:29 +0800

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  base:'./',
+  plugins: [vue()],
+  base: './',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
